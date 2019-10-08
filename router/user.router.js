6fb28c7e9073e3fd8edc01578aee9694e38ae027
Pranav Ruparelia <pranavruparelia9@gmail.com>
2019-10-08 11:49:31 -0400

    Added User search with User Email Address

--- router/user.router.js
+++ router/user.router.js
@@ -9,16 +9,25 @@ const { User, validate } = require('../models/user.model.js');
 
 router.use(express.json());
 
+//Gets all the Users required
 router.get('/', async (request, response) => {
     const user = await User.find();
     response.send(user);
 });
 
+//Gets the User matching with the required id 
 router.get('/:username', async (request, response) => {
     const user = await User.findOne({ username: request.params.username});
     response.send(_.pick(user, ["id", "username", "email", "contact"]));
 });
 
+//Gets the User matching with the required id 
+router.get('/:email', async (request, response) => {
+    const user = await User.findOne({ email: request.params.email});
+    response.send(_.pick(user, ["id", "username", "email", "contact"]));
+});
+
+//Allows the User to Sign Up themselves in the Database
 router.post('/', async (request, response) => {
 
     //Check the Error
@@ -49,7 +58,8 @@ router.post('/', async (request, response) => {
     const salt = await bcrypt.genSalt(10);
     newUser.password =  await bcrypt.hash(newUser.password, salt);
     
-    await newUser.save()
+    await newUser.save();
+
     const token =
         jwt.sign(
             {
@@ -63,9 +73,11 @@ router.post('/', async (request, response) => {
         .status(200).send(_.pick(newUser, ['id', 'username']));
 })
 
+//Change User Password
 router.put('/userPass/:id', async (request, response) => {
     
     const user = await User.findById(request.params.id);
+    //Compare the Old and new Pasword
     if(!await bcrypt.compare(request.body.password, user.password)) {
         console.log(!await bcrypt.compare(request.body.password, user.password));
         
@@ -73,8 +85,8 @@ router.put('/userPass/:id', async (request, response) => {
         const salt = await bcrypt.genSalt(10);
         request.body.password =  await bcrypt.hash(request.body.password, salt);
 
-        const user_update = await User.findByIdAndUpdate({_id : request.params.id} , {
-            
+        //Updates the Database and sends the response to the server
+        const user_update = await User.findByIdAndUpdate({_id : request.params.id} , {            
             $set: {
                 username: request.body.username,
                 email: request.body.email,
