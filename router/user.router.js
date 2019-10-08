32aa9927893deb035db5af396820648f8dc1e04d
Pranav Ruparelia <pranavruparelia9@gmail.com>
2019-10-08 02:35:28 -0400

    Added Update Password Field API

--- router/user.router.js
+++ router/user.router.js
@@ -16,7 +16,7 @@ router.get('/', async (request, response) => {
 
 router.get('/:username', async (request, response) => {
     const user = await User.findOne({ username: request.params.username});
-    response.send(_.pick(user, ["username", "email", "contact"]));
+    response.send(_.pick(user, ["id", "username", "email", "contact"]));
 });
 
 router.post('/', async (request, response) => {
@@ -47,7 +47,7 @@ router.post('/', async (request, response) => {
 
     //Encrypt Password using Bcrypt
     const salt = await bcrypt.genSalt(10);
-    newUser.password = await bcrypt.hash(newUser.password, salt);
+    newUser.password =  await bcrypt.hash(newUser.password, salt);
     
     await newUser.save()
     const token =
@@ -62,5 +62,33 @@ router.post('/', async (request, response) => {
         .header('x-auth-token', token)
         .status(200).send(_.pick(newUser, ['id', 'username']));
 })
+
+router.put('/userPass/:id', async (request, response) => {
+    
+    const user = await User.findById(request.params.id);
+    if(!await bcrypt.compare(request.body.password, user.password)) {
+        console.log(!await bcrypt.compare(request.body.password, user.password));
+        
+        //Encrypt Password using Bcrypt
+        const salt = await bcrypt.genSalt(10);
+        request.body.password =  await bcrypt.hash(request.body.password, salt);
+
+        const user_update = await User.findByIdAndUpdate({_id : request.params.id} , {
+            
+            $set: {
+                username: request.body.username,
+                email: request.body.email,
+                password: request.body.password,
+                contact: request.body.contact,
+                cart: request.body.cart,
+                userType: request.body.userType,
+            }
+        })        
+        response.status(200)
+        .send("Password successfully updated for user with username: " + user_update.username);
+    } else {
+        return response.status(400).send("You cannot enter last 3 Passwords");
+    }
+});
 
 module.exports = router;
